4f5e5def8f0afb6e3346a5ec663cb8a9ad94f0d0
zielaskowski <michal.zielaskowski@gmail.com>
2026-04-10 00:51:03 +0200

    removed jsregexp due issue with updating

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -14,7 +14,7 @@ return { -- Autocompletion
         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
           return
         end
-        return 'make install_jsregexp'
+        --return 'make install_jsregexp'
       end)(),
       dependencies = {
         -- `friendly-snippets` contains a variety of premade snippets.
